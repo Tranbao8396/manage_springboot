8f3b6b84e60fe70f097b3375111921265ef0beba
Tran Ngoc Bao <baotn@icd-vn.com>
2024-11-26 15:40:05 +0700

    wip

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,6 +1,22 @@
+var mode_init = localStorage.getItem('mode');
+
+if (mode_init == 'active') {
+  $('.side-menu').addClass('active');
+  $('#side_hamburger_expand').addClass('active');
+}
+
 $('#side_hamburger_expand').on('click', function () {
-  $('.side-menu').toggleClass('active');
-  $(this).toggleClass('active');
+  var mode = localStorage.getItem('mode');
+
+  if (mode == '' || mode == null) {
+    $('.side-menu').addClass('active');
+    $(this).addClass('active');
+    localStorage.setItem('mode', 'active'); ;
+  } else {
+    $('.side-menu').removeClass('active');
+    $(this).removeClass('active');
+    localStorage.setItem('mode', '');
+  }
 })
 
 $('body').on('click', function() {
